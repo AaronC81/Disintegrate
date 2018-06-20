b83d899475f09a2a34962b628cae0fc5965f0522
Aaron Christiansen <aaronc20000@gmail.com>
2018-06-20 23:10:15 +0100

    Improve stability of HearthstoneConfigurator

--- Disintegrate/Configuration/Configurators/HearthstoneConfiguator.cs
+++ Disintegrate/Configuration/Configurators/HearthstoneConfiguator.cs
@@ -4,30 +4,68 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IniParser;
 
 namespace Disintegrate.Configuration.Configurators
 {
-    // TODO: This works, but is quite destructive.
-    //       Could do with an INI reader/writer to implement this properly.
     public class HearthstoneConfiguator : Configurator
     {
-        const string Config = @"[Power]
-LogLevel=1
-FilePrinting=true
-ConsolePrinting=true
-ScreenPrinting=false
-Verbose=true";
-
         private string _logConfigLocation = Environment.ExpandEnvironmentVariables("%LOCALAPPDATA%/Blizzard/Hearthstone/log.config");
 
         public override List<string> Configure()
         {
-            
-            File.WriteAllText(_logConfigLocation, Config);
+            // If the file already exists, read it, otherwise just create a blank one
+            IniParser.Model.IniData data;
+            if (File.Exists(_logConfigLocation))
+            {
+                data = new FileIniDataParser().ReadFile(_logConfigLocation);
+            }
+            else
+            {
+                data = new IniParser.Model.IniData();
+            }
+
+            // Create the [Power] section if it doesn't exist
+            if (!data.Sections.ContainsSection("Power"))
+            {
+                data.Sections.AddSection("Power");
+            }
+
+            // Set things appropriately
+            data.Sections["Power"]["LogLevel"] = "1";
+            data.Sections["Power"]["Verbose"] = "true";
+            data.Sections["Power"]["FilePrinting"] = "true";
+
+            // Write back
+            File.WriteAllText(_logConfigLocation, data.ToString());
             return new List<string> { _logConfigLocation };
         }
 
-        public override bool IsConfigured() =>
-            File.Exists(_logConfigLocation) && File.ReadAllText(_logConfigLocation) == Config;
+        public override bool IsConfigured()
+        {
+            // Check that the file actually exists
+            if (!File.Exists(_logConfigLocation))
+            {
+                return false;
+            }
+
+            // Check that it contains a [Power] section
+            var parsedIni = new FileIniDataParser().ReadFile(_logConfigLocation);
+
+            if (!parsedIni.Sections.ContainsSection("Power"))
+            {
+                return false;
+            }
+
+            // Check that it has LogLevel=1, FilePrinting=true and Verbose=true
+            var powerSection = parsedIni.Sections["Power"];
+
+            bool EqualsIfExists(string key, string expected) =>
+                powerSection.ContainsKey(key) && powerSection[key].ToLower() == expected.ToLower();
+
+            return EqualsIfExists("Verbose", "true")
+                && EqualsIfExists("LogLevel", "1")
+                && EqualsIfExists("FilePrinting", "true");            
+        }
     }
 }
